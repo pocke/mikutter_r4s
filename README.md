63ce352dd86275b8e784448821672f11030789b9
pocke <p.ck.t22@gmail.com>
2013-11-11 14:18:38 +0900

    add license

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 mikutter_r4s
 ============
-
+licensed MIT  
 mikutterからr4sをキメるプラグインです。
--- r4s.rb
+++ r4s.rb
@@ -1,5 +1,9 @@
 #-*- coding: utf-8 -*-
 
+# Copyright 2013, pocket
+# Licensed MIT
+# http://opensource.org/licenses/mit-license.php
+
 Plugin.create(:r4s) do
   command(:r4s,
     name: 'r4s',
