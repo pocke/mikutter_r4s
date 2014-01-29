cdaaca8ea168d2553f3cc2666adc7c0624c2c362
pocke <p.ck.t22@gmail.com>
2014-01-29 16:14:44 +0900

    昔書いたコードがきもかった

--- r4s.rb
+++ r4s.rb
@@ -1,18 +1,20 @@
 #-*- coding: utf-8 -*-
 
-# Copyright 2013, pocket
+# Copyright 2013-2014, pocke
 # Licensed MIT
 # http://opensource.org/licenses/mit-license.php
 
 Plugin.create(:r4s) do
-  command(:r4s,
+  command(
+    :r4s,
     name: 'r4s',
-    description: '(ﾟ∀ﾟ)o彡゜スパブロ!スパブロ!',
+    description: 'r4s',
     condition: lambda{|x| true},
     visible: true,
-    role: :timeline) do |opt|
+    role: :timeline
+  ) do |opt|
     opt.messages.each do |m|
-      (Service.primary.twitter/'users/report_spam').json(:screen_name => m.user.idname)
+      (Service.primary.twitter/'users/report_spam').json(screen_name: m.user.idname)
     end
   end
 end
